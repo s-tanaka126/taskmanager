3efc97924c29643d3d42ddb438debb8f7403c93c
s-tanaka126 <omotya.hako@gmail.com>
2021-07-10 20:17:53 +0900

    first commit

---
+++ src/test/java/TaskManager/application/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package TaskManager.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
