fb842cfd944875bb9fe4a34e0c1e9ffe0f9e41af
s-tanaka126 <omotya.hako@gmail.com>
2021-07-17 22:16:41 +0900

    ControllerとRequestとEntityとServiceとDao作成

---
+++ src/main/java/TaskManager/application/Controller/TaskController.java
@@ -0,0 +1,38 @@
+package TaskManager.application.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import TaskManager.application.Entity.TaskEntity;
+import TaskManager.application.Request.TaskRequest;
+import TaskManager.application.Service.TaskService;
+
+@Controller
+@RequestMapping("/task")
+public class TaskController {
+	
+	  @Autowired
+	  TaskService taskService;
+	  
+	@GetMapping	("/list")
+	public String taskForm(TaskRequest taskRequest) {
+
+		return "/task/list";
+
+	}
+
+	@PostMapping("/complete")
+	public String taskComplete(TaskRequest taskRequest) {
+		
+		  TaskEntity taskEntity = new TaskEntity();
+		  
+		  taskEntity.setTaskName(taskRequest.getTaskName());
+		  
+		  taskService.taskContentInsert(taskEntity);
+
+		return "redirect:/task/list";
+	}
+}
---
+++ src/main/java/TaskManager/application/Service/TaskService.java
@@ -0,0 +1,25 @@
+package TaskManager.application.Service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import TaskManager.application.Dao.TaskDao;
+import TaskManager.application.Entity.TaskEntity;
+
+@Service
+@Transactional
+public class TaskService {
+	
+	@Autowired
+	TaskDao taskInsert;
+	
+	public void taskContentInsert(TaskEntity taskEntity) {
+		
+		taskInsert.taskContentInsert(taskEntity);
+	}
+	
+	
+
+}
