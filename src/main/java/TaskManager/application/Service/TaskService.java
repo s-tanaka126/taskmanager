5fcdf658f01cf466b31f1d2d55cc8c5762a4459b
s-tanaka126 <omotya.hako@gmail.com>
2021-07-31 20:13:58 +0900

    変更点プッシュ２

--- src/main/java/TaskManager/application/Service/TaskService.java
+++ src/main/java/TaskManager/application/Service/TaskService.java
@@ -5,25 +5,22 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-<<<<<<< HEAD
-=======
+
 import TaskManager.application.Dao.TaskDao;
 import TaskManager.application.Entity.TaskEntity;
 
->>>>>>> c1362f4b71a302bb5d608c9a46a67c9bac66cb98
+
 @Service
 @Transactional
 public class TaskService {
 	
 	@Autowired
-<<<<<<< HEAD
 
-=======
 	TaskDao taskInsert;
 	
 	public void taskContentInsert(TaskEntity taskEntity) {
 		
 		taskInsert.taskContentInsert(taskEntity);
 	}
->>>>>>> c1362f4b71a302bb5d608c9a46a67c9bac66cb98
+
 }
